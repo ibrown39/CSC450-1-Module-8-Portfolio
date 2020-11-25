3c6a1f657c200d68c42063130653a25c1ce28d91
ibrown39 <keyblademan@yahoo.com>
2020-11-25 16:51:51 -0700

    Update anotherWayToCreateThreads.java

--- anotherWayToCreateThreads.java
+++ anotherWayToCreateThreads.java
@@ -1,31 +1,47 @@
 
-//I tested this on https://www.onlinegdb.com/online_java_compiler
 public class Main {
-    public static class exampleThreadClass extends Thread {
+    public static class exampleThreadClass1 extends Thread {
         public void run(){
-            System.out.println("I am a thread via extends Thread");
+            System.out.println("I am a thread via (new exampleThreadClass1()).start(); and extend thread");
         }
         
     }
     
-    public static class exampleThreadClass2 implements Runnable{
+     public static class exampleThreadClass2 implements Runnable {
         public void run(){
-            System.out.println("I am a thread via implementing Runnable");
+            System.out.println("I am a thread via (new Thread(new exampleThreadClass2())).start(); and implementing runnable");
         }
+        
     }
     
     public static class exampleThreadClass3 implements Runnable{
         public void run(){
-            System.out.println("I am a thread via implementing Runnable and using an object");
+            System.out.println("I am a thread via Thread t3 = new Thread(new exampleThreadClass3()); and implementing Runnable");
+        }
+    }
+    
+    public static class exampleThreadClass4 implements Runnable{
+        public void run(){
+            System.out.println("I am a thread via implementing Runnable and using an object (eTC4)");
+        }
+    }
+    
+    public static class exampleThreadClass5 extends Thread{
+        public void run(){
+            System.out.println("I am a thread via extend Thread and using an object (eTC5)");
         }
     }
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		(new exampleThreadClass()).start(); //t1
-		Thread t2 = new Thread(new exampleThreadClass2());
-		exampleThreadClass3 eTC3 = new exampleThreadClass3();
-		Thread t3 = new Thread(eTC3);
+		(new exampleThreadClass1()).start(); //t1
+		(new Thread(new exampleThreadClass2())).start(); //t2
+		Thread t3 = new Thread(new exampleThreadClass3()); //t3
+		exampleThreadClass4 eTC4 = new exampleThreadClass4(); //obj that is instance of exampleThreadClass3
+		Thread t4 = new Thread(eTC4); //t4
+		exampleThreadClass5 eTC5 = new exampleThreadClass5();////obj that is instance of exampleThreadClass5
+		Thread t5 = new Thread(eTC5);//t5
+		
 		
 		
 }
